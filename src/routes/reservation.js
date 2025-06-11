e04cd86e4d36a78ab7b8139da1e6fd0b23885c7c
efrain <efrainperalta.f@gmail.com>
2025-06-11 01:35:35 -0400

    fix: corregir rutas

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -3,9 +3,9 @@ const { ReservationController } = require("../controllers/reservation");
 
 const reservationRouter = Router()
 
-reservationRouter.post('/', ReservationController.createReservation)
-reservationRouter.patch('/:id/state', ReservationController.updateReservationState)
-reservationRouter.delete('/:id', ReservationController.deleteReservation)
+reservationRouter.post('/reservations', ReservationController.createReservation)
+reservationRouter.patch('/reservations/:id/state', ReservationController.updateReservationState)
+reservationRouter.delete('/reservations/:id', ReservationController.deleteReservation)
 // Verificar si un usuario hizo la reserva de un carro
-reservationRouter.get('/verificar/:idUsuario/:idCarro', ReservationController.verificarReservaUsuario);
+reservationRouter.get('/reservations/verificar/:idUsuario/:idCarro', ReservationController.verificarReservaUsuario);
 module.exports = { reservationRouter }
